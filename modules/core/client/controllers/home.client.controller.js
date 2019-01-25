e018c5aff2c0b21c5d56b84e3e4fc61a8e88ea8f
Erik Larsson <eriklarsson9@hotmail.com>
2019-01-25 01:26:14 +0100

    .

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,11 +1,22 @@
-(function () {
-  'use strict';
+angular.module('invoice1', [])
+.controller('InvoiceController', function InvoiceController() {
+  this.qty = 1;
+  this.cost = 2;
+  this.inCurr = 'EUR';
+  this.currencies = ['USD', 'EUR', 'CNY'];
+  this.usdToForeignRates = {
+    USD: 1,
+    EUR: 0.74,
+    CNY: 6.09
+  };
 
-  angular
-    .module('core')
-    .controller('HomeController', HomeController);
-
-  function HomeController() {
-    var vm = this;
-  }
-}());
+  this.total = function total(outCurr) {
+    return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
+  };
+  this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
+    return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
+  };
+  this.pay = function pay() {
+    window.alert('Thanks!');
+  };
+});
