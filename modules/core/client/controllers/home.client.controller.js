83ce8972094c47b8cea222979fdf740a61fb1b8b
Erik Larsson <eriklarsson9@hotmail.com>
2019-01-25 02:12:58 +0100

    .

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,23 +1,46 @@
-angular.module('core', [])
-.controller('HomeController', function HomeController() {
-  var vm = this;
-  this.qty = 1;
-  this.cost = 2;
-  this.inCurr = 'EUR';
-  this.currencies = ['USD', 'EUR', 'CNY'];
-  this.usdToForeignRates = {
-    USD: 1,
-    EUR: 0.74,
-    CNY: 6.09
-  };
+(function () {
+  'use strict';
 
-  this.total = function total(outCurr) {
-    return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
-  };
-  this.convertCurrency = function convertCurrency(amount, inCurr, outCurr) {
-    return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
-  };
-  this.pay = function pay() {
-    window.alert('Thanks!');
-  };
-});
+  angular
+    .module('core')
+    .controller('HomeController', HomeController);
+
+  function HomeController() {
+    var vm = this;
+    //var mongoose = require("../../../config/lib/mongoose");
+    var mongoose = require('mongoose');
+    mongoose.connect(process.env.MONGOLAB_URI);
+    var db = mongoose.connection;
+    mongoose.connect();
+    mongoose = require('mongoose');
+    var kittySchema = new mongoose.Schema({
+  		name: String
+	});
+	kittySchema.methods.speak = function () {
+  	var greeting = this.name
+    	? "Meow name is " + this.name
+    	: "I don't have a name";
+  		console.log(greeting);
+	}
+
+	var Kitten = mongoose.model('Kitten', kittySchema);
+
+	var silence = new Kitten({ name: 'Silence' });
+	console.log(silence.name);
+
+	var fluffy = new Kitten({ name: 'fluffy' });
+	fluffy.speak(); // "Meow name is fluffy"
+
+	fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    	fluffy.speak();
+  	});
+
+  	Kitten.find(function (err, kittens) {
+  		if (err) return console.error(err);
+  	console.log(kittens);
+	})
+
+
+  }
+}());
